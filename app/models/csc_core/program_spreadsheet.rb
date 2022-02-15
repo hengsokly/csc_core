7a588ba31a3604aef6005a52569fe36c717ed9f2
Sokly <hengsokly23@gmail.com>
2022-02-15 11:50:30 +0700

    Local ngo: add program spreadsheet

---
+++ app/models/csc_core/program_spreadsheet.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module CscCore
+  class ProgramSpreadsheet
+    attr_reader :program_id
+
+    def initialize(program_id)
+      @program_id = program_id
+    end
+
+    def import(file_path)
+      spreadsheet(file_path).each_with_pagename do |sheet_name, sheet|
+        get(sheet_name).import(sheet)
+      rescue
+        Rails.logger.warn "unknown handler for sheet: #{sheet_name}"
+      end
+    end
+
+    private
+      def get(sheet_name)
+        "CscCore::Spreadsheets::#{sheet_name.camelcase}Spreadsheet".constantize.new(program_id)
+      end
+
+      def spreadsheet(file_path)
+        Roo::Spreadsheet.open(file_path)
+      end
+  end
+end
