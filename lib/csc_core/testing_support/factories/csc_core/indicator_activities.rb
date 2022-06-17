393e2acaa9518544909de767575865498b9e0fe9
Sokly <hengsokly23@gmail.com>
2022-06-17 15:19:23 +0700

    Pdf interpreter: add primary school name with facility name

--- lib/csc_core/testing_support/factories/csc_core/indicator_activities.rb
+++ lib/csc_core/testing_support/factories/csc_core/indicator_activities.rb
@@ -21,4 +21,20 @@ FactoryBot.define do
     voting_indicator
     scorecard
   end
+
+  # Todo: remove it after web portal is on production
+  factory :tmp_strength_indicator_activity, class: "::StrengthIndicatorActivity" do
+    content { FFaker::Name.name }
+    voting_indicator_uuid { create(:voting_indicator).uuid }
+  end
+
+  factory :tmp_weakness_indicator_activity, class: "::WeaknessIndicatorActivity" do
+    content { FFaker::Name.name }
+    voting_indicator_uuid { create(:voting_indicator).uuid }
+  end
+
+  factory :tmp_suggested_indicator_activity, class: "::SuggestedIndicatorActivity" do
+    content { FFaker::Name.name }
+    voting_indicator_uuid { create(:voting_indicator).uuid }
+  end
 end
